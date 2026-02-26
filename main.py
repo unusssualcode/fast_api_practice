2d4958fd84e3dd187726f563a8e23821a6ceb634
Denis <densmm@ukr.net>
2026-02-26 19:26:01 +0200

    start learning

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body
 import uvicorn
 
 
@@ -6,17 +6,72 @@ app = FastAPI()
 
 
 hotels = [
-    {"id":1, "title":"Reni"},
-    {"id":2, "title":"Izmail"}
+    {"id":1, "title":"Reni", "name":"reni"},
+    {"id":2, "title":"Izmail", "name":"izmail"}
 ]
 
 
 @app.get("/hotels")
 def get_hotels(
-    title:str
+    id: int | None = Query(None, description="Id"),
+    title:str | None = Query(None, description="Title")
 ):
-    return [hotel for hotel in hotels if hotel["title"] == title]
+    hotels_ =[]
+    for hotel in hotels:
+        if title and hotel["title"] != title:
+            continue
+        if id and hotel["id"] != id:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(
+    title:str = Body(embed=True)
+):
+    global hotels
+    hotels.append({
+        "id":hotels[-1]["id"]+1,
+        "title":title
+    })
+    return {"status":"OK"}
+
+
+@app.patch("/hotels/{hotel_id}", summary="Partial editing hotels", description="Here you can update info about hotels")
+def edit_hotel(
+        hotel_id:int, 
+        title:str | None = Body(None), 
+        name:str | None = Body(None)
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"]==hotel_id][0]
+    if title:
+        hotel["title"] = title
+    if name:
+        hotel["name"] = name
+    return {"status":"OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def partially_edit_hotel(
+        hotel_id:int, 
+        title:str = Body(), 
+        name:str = Body()
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"]==hotel_id][0]
+    hotel["title"] = title
+    hotel["name"] = name
+    return {"status":"OK"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status":"OK"}
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app", reload=True)g
+    uvicorn.run("main:app", reload=True)
