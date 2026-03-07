204d0521e693c132e66cd9ae8e6639fe57ae80b6
Denis <densmm@ukr.net>
2026-03-07 16:43:32 +0200

    Hotels Model creation

--- src/database.py
+++ src/database.py
@@ -1,10 +1,15 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
-
-import asyncio
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.DB_URL)
 
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+session = async_session_maker()
+
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/models/hotels.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+from src.database import Base
+
+
+class HotelsOrm(Base):
+    __tablename__ = "hotels"
+    
+    
+    id: Mapped[int]  = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    location: Mapped[str]
+    
