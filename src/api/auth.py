9c2cbc6d98a3c4f3e18742ef0d6823cdabbda196
Denis <densmm@ukr.net>
2026-05-11 19:37:04 +0300

    auth realization

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,36 +1,22 @@
 from datetime import datetime, timedelta, timezone
 
-import jwt
-from fastapi import APIRouter, HTTPException
-from pwdlib import PasswordHash
 
+from fastapi import APIRouter, HTTPException, Response
+
+from services.auth import AuthService
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from schemas.users import UserAdd, UserRequestAdd
 
+
 router = APIRouter(prefix="/auth", tags=["Users Autorization and authentification"])
-
-
-password_hash = PasswordHash.recommended()
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= ({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @router.post("/register")
 async def register_user(
     data: UserRequestAdd
 ):
-    hashed_password = password_hash.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email = data.email, hashed_password = hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
@@ -41,11 +27,15 @@ async def register_user(
 
 @router.post("/login")
 async def login_user(
-    data: UserRequestAdd
+    data: UserRequestAdd,
+    response: Response
 ):
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email = data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(email = data.email)
         if not user:
             raise HTTPException(status_code = 401, detail = "No User with this email")
-        access_token = create_access_token({"user_id": user.id})
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code = 401, detail = "Incorrect password")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,8 +1,18 @@
+from pydantic import EmailStr
+from sqlalchemy import select
+
 from models.users import UsersOrm
 from repositories.base import BaseRepository
-from schemas.users import User
+from schemas.users import User, UserWithHashedPassword
 
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+    
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email = email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model)
+        
